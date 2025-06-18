17b19055336a598927d10533c4304643736b3eb2
alsawaqi <buzz644@yahoo.com>
2025-06-18 19:36:07 +0000

    updated for production

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,4 +1,29 @@
 <script setup lang="ts">
+
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css'
+    },
+       { rel: 'stylesheet', href: '/isc-assets/css/style.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/remixicon.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/bootstrap.min.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/apexcharts.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/dataTables.min.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/flatpickr.min.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/full-calendar.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/jquery-jvectormap-2.0.5.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/magnific-popup.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/slick.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/prism.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/file-upload.css' },
+    { rel: 'stylesheet', href: '/isc-assets/css/lib/audioplayer.css' },
+    { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.16.4/dist/katex.min.css' }
+ 
+    
+  ]
+})
  
  if (process.client) {
     const scripts = [
--- nuxt-app/nuxt.config.ts
+++ nuxt-app/nuxt.config.ts
@@ -12,29 +12,11 @@ export default defineNuxtConfig({
     autoImport: true,
   },
  
-   css: [
-    '~/public/isc-assets/css/style.css',
-    '~/public/isc-assets/css/remixicon.css',
-    '~/public/isc-assets/css/lib/bootstrap.min.css',
-    '~/public/isc-assets/css/lib/apexcharts.css',
-    '~/public/isc-assets/js/lib/iconify-icon.min.js',
-    '~/public/isc-assets/css/lib/dataTables.min.css',
-    '~/public/isc-assets/css/lib/editor-katex.min.css',
-    '~/public/isc-assets/css/lib/editor.atom-one-dark.min.css',
-    '~/public/isc-assets/css/lib/editor.quill.snow.css',
-    '~/public/isc-assets/css/lib/flatpickr.min.css',
-    '~/public/isc-assets/css/lib/full-calendar.css',
-    '~/public/isc-assets/css/lib/jquery-jvectormap-2.0.5.css',
-    '~/public/isc-assets/css/lib/magnific-popup.css',
-    '~/public/isc-assets/css/lib/slick.css',
-    '~/public/isc-assets/css/lib/prism.css',
-    '~/public/isc-assets/css/lib/file-upload.css',
-    '~/public/isc-assets/css/lib/audioplayer.css'
-  ],
+   css: [],
   plugins: ['~/plugins/bootstrap.client.ts'],
    runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:81' // your Laravel base URL
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'https://api.abdallahweb.com' // your Laravel base URL
     }
   },
   compatibilityDate: '2025-03-25'
