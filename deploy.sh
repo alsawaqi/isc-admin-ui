0370f137747fd7898285564cd2d11cae2cbf07af
Abdallah Al Sawaqi <buzz644@yahoo.com>
2025-06-21 13:51:54 +0400

    test

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 echo "🚀 Building Nuxt SSR Docker container..."
 docker compose -f docker-compose.prod.yml down
 docker compose --env-file .env.production -f docker-compose.prod.yml up -d --build
