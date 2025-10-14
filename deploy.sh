3f2d0fd3ea0e5686f0873369e885bbe8de329b9a
Abdallah Al Sawaqi <buzz644@yahoo.com>
2025-10-15 02:25:09 +0400

    updated the url in the delpoy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 echo "🚀 Building Nuxt SSR Docker container..."
 docker compose -f docker-compose.prod.yml down
 docker compose --env-file .env.production -f docker-compose.prod.yml up -d --build
-echo "✅ Deployed at: https://abdallahweb.com"
+echo "✅ Deployed at: https://avinaq.com"
